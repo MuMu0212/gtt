9ad5380b12f4ccfb74fe1b1fe9e7ae1a50535e88
mumu <553277884@qq.com>
2019-06-21 08:55:24 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/','StaticPagesController@home');
+Route::get('/help','StaticPagesController@help');
+Route::get('/about','StaticPagesController@about');
 
 
    
